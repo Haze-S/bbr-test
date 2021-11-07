01cf4f06882e32d0973bd8adf8771fc905df0b77
Haze-S <skdml20022@gmail.com>
2021-11-07 23:20:46 +0900

    수정된 API 참고해서 route 생성

--- app.py
+++ app.py
@@ -6,36 +6,42 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/start')
+@app.route('/start', methods=['GET'])
 def show_total():
+    # pymongo 집계 함수? 찾아 적용
+    # secret.db.users.aggregate([
+    #         {'$group': {
+    #             _id: '$type',
+    #             total: {$sum: '$count'}
+    #             }
+    #         }
+    #     ])
     total = secret.db.users.count()
     return jsonify({'total': total})
 
-@app.route('/test')
+@app.route('/test', methods=['GET'])
 def test_html():
     return render_template('test.html')
 
-@app.route('/test')
+@app.route('/quest', methods=['GET'])
 def get_test():
     question = list(secret.db.questions.find({}, {'_id': False}))
     return jsonify({'quest': question})
 
-@app.route('/result')
-def show_result():
-    return render_template('result.html')
-
 @app.route('/result', methods=['GET', 'POST'])
-def result():
-    # if
-    number_receive = request.form['number_give']
-    type_receive = request.form['type_give']
-    doc = {
-        "number": number_receive,
-        "type": type_receive
-    }
-    secret.db.users.insert_one(doc)
-    type = list(secret.db.types.find_one({}, {'_id': False}))
-    return jsonify({'result': type})
+def show_result():
+    if(request.method == 'GET'):
+        return render_template('result.html')
+    elif(request.method == 'POST'):
+        type_receive = request.form['type_give']
+        # pymongo type_receive와 일치하는 column 찾아서 count++
+
+@app.route('/result/type')
+def detail():
+    keyword = request.args.get('type')
+    result_type = list(secret.db.types.find({'type': {'$regex': keyword}}))
+    return jsonify({'result': result_type})
+
 
 @app.errorhandler(404)
 def page_not_found(error):
